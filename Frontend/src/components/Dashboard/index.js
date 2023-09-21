60b35653feae32b9920a3d9d16522489091ed8da
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-09-21 14:14:59 -0300

    transaction table styles

--- Frontend/src/components/Dashboard/index.js
+++ Frontend/src/components/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
 import { Sumary } from "../Sumary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export const Dashboard = () => {
   return (
     <>
       <div className="containerDash">
         <Sumary />
+        <TransactionsTable />
       </div>
     </>
   );
--- Frontend/src/components/TransactionsTable/index.js
+++ Frontend/src/components/TransactionsTable/index.js
@@ -1,5 +1,35 @@
 import React from "react";
-
-export const index = () => {
-  return <div>index</div>;
+import "../TransactionsTable/style.css";
+export const TransactionsTable = () => {
+  return (
+    <div className="containerTable">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Quantidade</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Venda de livros</td>
+            <td className="deposit">R$200,00</td>
+            <td>Comida</td>
+            <td>null</td>
+            <td>21/09/23</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$300,00</td>
+            <td>Comida</td>
+            <td>null</td>
+            <td>10/09/23</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
