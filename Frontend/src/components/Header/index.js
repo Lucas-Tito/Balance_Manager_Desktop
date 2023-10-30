993865bedea96071dfa0e443e039c613619c0f20
Lucas Tito <lucassga500@gmail.com>
2023-10-30 14:47:43 -0300

    added hamburguer icon

--- Frontend/src/components/Header/index.js
+++ Frontend/src/components/Header/index.js
@@ -1,10 +1,12 @@
 import totalIcon from "../../assets/totalIcon.png";
+import menu_icon from "../../assets/menu_icon.svg"
 import "./../Header/style.css";
 import Modal from "react-modal";
 
 export const Header = ({ openModal}) => {
   return (
     <div className="container">
+      <img src={menu_icon} alt="logo" className="menu_icon"/>
       <div className="content">
         <img src={totalIcon} alt="logo" />
         <button className="btn1" onClick={openModal}>
