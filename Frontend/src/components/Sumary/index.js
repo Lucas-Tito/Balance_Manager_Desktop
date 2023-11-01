10ec9fcfa0284c3ae6154144e8c7370e22112d18
Lucas Tito <lucassga500@gmail.com>
2023-11-01 06:52:23 -0300

    tried to add introjs, but no success

--- Frontend/src/components/Sumary/index.js
+++ Frontend/src/components/Sumary/index.js
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
 import "./style.css";
-
 import incomeIcon from "../../assets/incomeIcon.png";
 import expensesIcon from "../../assets/expensesIcon.png";
 import totalIcon from "../../assets/totalIcon.png";
 import { TransactionsContext } from "../../TransactionContext";
+import 'intro.js/introjs.css';
+import { Steps } from 'intro.js-react';
+import {useState} from 'react';
 
 export const Sumary = () => {
   const transactions = useContext(TransactionsContext);
@@ -27,46 +29,72 @@ export const Sumary = () => {
     }
   );
 
+  //Onboarding code
+  const [enabled, setEnabled] = useState(true)
+  const [initialStep, setInitialStep] = useState(0)
+
+  const onExit = () => {};
+  const steps = [
+    {
+      element: '.containerSumary',
+      intro: 'Aqui você encontrará a situação atual do seu montante',
+      position: 'top',
+    },
+  ];
+  
+
+
   return (
-    <div className="containerSumary">
-      <div className="box">
-        <header className="header">
-          <p>Entradas</p>
-          <img src={incomeIcon} alt="incomeIcone" />
-        </header>
-        <strong className="strong">
-          {new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(sumary.income)}
-        </strong>
+    <>
+
+      <div className="containerSumary">
+            {/* Onboarding component */}
+        <Steps
+          enabled={true}
+          steps={steps}
+          initialStep={0}
+          onExit={onExit}
+        />
+
+        <div className="box">
+          <header className="header">
+            <p>Entradas</p>
+            <img src={incomeIcon} alt="incomeIcone" />
+          </header>
+          <strong className="strong">
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(sumary.income)}
+          </strong>
+        </div>
+        <div className="box">
+          <header className="header">
+            <p>Saídas</p>
+            <img src={expensesIcon} alt="expenses" />
+          </header>
+          <strong className="strong">
+            {" "}
+            -{" "}
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(sumary.expenses)}
+          </strong>
+        </div>
+        <div className="boxTotal">
+          <header className="header">
+            <p>Total</p>
+            <img src={totalIcon} alt="total" />
+          </header>
+          <strong className="strong">
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(sumary.total)}
+          </strong>
+        </div>
       </div>
-      <div className="box">
-        <header className="header">
-          <p>Saídas</p>
-          <img src={expensesIcon} alt="expenses" />
-        </header>
-        <strong className="strong">
-          {" "}
-          -{" "}
-          {new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(sumary.expenses)}
-        </strong>
-      </div>
-      <div className="boxTotal">
-        <header className="header">
-          <p>Total</p>
-          <img src={totalIcon} alt="total" />
-        </header>
-        <strong className="strong">
-          {new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(sumary.total)}
-        </strong>
-      </div>
-    </div>
+    </>
   );
 };
